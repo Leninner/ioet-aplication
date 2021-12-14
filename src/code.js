25f614f3397cab838e54993b2ce6bf51256de30a
leninner <mazabandalenin180@gmail.com>
2021-12-14 16:53:27 -0500

    Commit inicial

---
+++ src/code.js
@@ -0,0 +1,32 @@
+// Exercise
+
+// The company ACME offers their employees the flexibility to work the hours they want. But due to some external circumstances they need to know what employees have been at the office within the same time frame
+
+// The goal of this exercise is to output a table containing pairs of employees and how often they have coincided in the office.
+
+// Input: the name of an employee and the schedule they worked, indicating the time and hours. This should be a .txt file with at least five sets of data. You can include the data from our examples below:
+
+// Example 1:
+
+// INPUT
+// RENE=MO10:00-12:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00- 21:00
+// ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00
+// ANDRES=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00
+
+// OUTPUT:
+// ASTRID-RENE: 2
+// ASTRID-ANDRES: 3
+// RENE-ANDRES: 2
+
+// Example 2:
+
+// INPUT:
+// RENE=MO10:15-12:00,TU10:00-12:00,TH13:00-13:15,SA14:00-18:00,SU20:00-21:00
+// ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00
+
+// OUTPUT:
+// RENE-ASTRID: 3
+
+export const code = () => {
+  return;
+};
---
+++ src/code.test.js
@@ -0,0 +1,7 @@
+import { code } from './code';
+
+describe('code', () => {
+  test('Debe funcionar', () => {
+    expect(code(5)).toBe(5);
+  });
+});

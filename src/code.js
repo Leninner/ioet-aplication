f32fe5d4a04c2994e55b03bde9e8e89e948473ac
Leninner <mazabandalenin180@gmail.com>
2021-12-15 13:51:04 -0500

    Update

--- src/code.js
+++ src/code.js
@@ -33,10 +33,16 @@ const PATH = 'C:\\Users\\USUARIO\\Desktop\\ioet-aplication\\src\\setsOfData.txt'
 
 const arrayOfData = fs.readFileSync(PATH, 'utf8').split('|');
 
-const getArray = () => {
+const some =
+  'RENE=MO10:00-12:00,TU10:00-12:00,TH01:00-03:00,SA14:00-18:00,SU20:00-21:00|ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00|ANDRES=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00';
+
+const yes =
+  'RENE=MO10:15-12:00,TU10:00-12:00,TH13:00-13:15,SA14:00-18:00,SU20:00-21:00|ASTRID=MO10:00-12:00,TH12:00-14:00,SU20:00-21:00';
+
+const getArray = (some) => {
   let obj = {};
 
-  arrayOfData.forEach((value) => {
+  some.split('|').forEach((value) => {
     const name = value.split('=')[0];
     const schedule = value.split('=')[1];
 
@@ -45,9 +51,15 @@ const getArray = () => {
     }
   });
 
-  // NOTE: Se puede utilizar destructuración de arreglos
+  Object.entries(obj).map(([key, value]) => {
+    obj[key] = value.map((value) => value.split('-'));
+  });
+
+  Object.entries(obj).map(([key, value]) => {
+    obj[key] = value.map((value) => value.map((value) => value.split(':')[0]).join('-'));
+  });
 
   return obj;
 };
 
-console.log(getArray());
+console.log(getArray(yes));
